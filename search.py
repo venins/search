964d2ecc98cd15d327ac76823dcd11b340a11144
vishal singh <join4m.com@gmail.com>
2015-11-07 16:20:40 +0530

    working basics

--- search.py
+++ search.py
@@ -12,17 +12,27 @@ data = file2string(file_name)
 data = data.split(".")
 data1 = {}
 for i in range(len(data)):
-	data1[i]=string2stem(data[i].split())
+	data1[i]=string2stem([line.decode('utf-8').strip() for line in data[i].split()])
 
 words = word2synonyms(word)
-#print words
 words1 = string2stem(words)
 k=[]
+kk=[]
 for i in range(len(words1)):
 	for j in range(len(data)):
 		if words1[i] in data1[j]:
-			if j not in k:
+			if j not in kk:
 				k.append(j)
-
-for i in k:
-	print data[i]
+				kk.append(j)
+	if i == 0:
+		if len(k)>0:
+			print str(len(k)) + " results found."
+			for ii in range(len(k)):
+				print "Result #", str(ii+1), " - ",  "\"", data[k[ii]], ".\"\n"
+			k = []
+	else:
+		if len(k)>0:
+			print str(len(k)) + " results found replacing " + "\"" + words[0] + " with synonym " + words[i] + "\"."
+			for ii in range(len(k)):
+				print "Result #", str(ii+1), " - ", "\"", data[k[ii]], ".\"\n",
+			k = []
--- testing.py
+++ testing.py
@@ -1,4 +1,8 @@
 from functions import *
 
-list = ["vishal", "kings", "mans"]
-print string2stem(list)
+data = file2string("test.txt")
+data = data.split(".")
+print data
+data1 = {}
+for i in range(len(data)):
+	data1[i]=string2stem([line.decode('utf-8').strip() for line in data[i].split()])
