38b9a4ff5f54101c5a055107e70f6343ac419d96
vishal singh <join4m.com@gmail.com>
2015-11-08 12:09:43 +0530

    working with the directory and files

--- search3.py
+++ search3.py
@@ -1,10 +1,54 @@
-import os
+import sys
 
 from functions.functions import *
 
-#files_in_dir = []
-vikas = all_files_in_dir("test")
-print vikas
-files = all_files_in_dir("test")
-words = ['help', 'angel','did']
-#wordwithsynonyms("test.txt",words)
+def main():
+	words = []
+	option = None
+	file_name = None
+	dir_name = None
+
+	#accepting arguments from the cmd
+	try:
+		if sys.argv[1] == "-f" or sys.argv[1] == "-d":
+			option = sys.argv[1]
+		else:
+			print "Please select \"-f\" for single text file or \"-d\" for Full directory search"
+	except:
+		print "Please select \"-f\" for single text file or \"-d\" for Full directory search"
+		sys.exit()
+
+	try:
+		if sys.argv[2].endswith(".txt") and option == "-f":
+			file_name = sys.argv[2]
+		elif option =="-d":
+			dir_name = sys.argv[2]
+		else:
+			print "please enter text file name or directory name with proper option"
+	except:
+		print "Please enter .txt file name"
+		sys.exit()
+
+	try:
+		if len(sys.argv) > 3:
+			for args in sys.argv[3:]:
+				if args not in words:
+					words.append(args)
+		else:
+			print "Please enter one or more words to search"
+	except:
+		print "Please enter one or more words to search"
+		sys.exit()
+
+	if option == "-f":
+		wordwithsynonyms(file_name, words)
+	elif option == "-d":
+		files_in_dir = []
+		files_in_dir = all_files_in_dir(dir_name)
+		for files in files_in_dir:
+			print "Searching in file " + files + "\n"
+			wordwithsynonyms(files, words)
+
+
+if __name__ == "__main__":
+	main()
