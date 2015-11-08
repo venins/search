ec646527c177657bf1bc132004557fdaad8b5a63
vishal singh <join4m.com@gmail.com>
2015-11-08 12:50:59 +0530

    removed cmd_Argv functions

--- functions/functions.py
+++ functions/functions.py
@@ -150,35 +150,3 @@ def all_files_in_dir(dir_name):
 	return files_in_dir
 
 
-def cmd_argv():
-	"""accepting arguments from cmd"""
-	try:
-		if sys.argv[1] == "-f" or sys.argv[1] == "-d":
-			option = sys.argv[1]
-		else:
-			print "Please select \"-f\" for single text file or \"-d\" for Full directory search"
-	except:
-		print "Please select \"-f\" for single text file or \"-d\" for Full directory search"
-		sys.exit()
-
-	try:
-		if sys.argv[2].endswith(".txt") and option == "-f":
-			file_name = sys.argv[2]
-		elif option =="-d":
-			dir_name = sys.argv[2]
-		else:
-			print "please enter text file name or directory name with proper option"
-	except:
-		print "Please enter .txt file name"
-		sys.exit()
-
-	try:
-		if len(sys.argv) > 3:
-			for args in sys.argv[3:]:
-				if args not in words:
-					words.append(args)
-		else:
-			print "Please enter one or more words to search"
-	except:
-		print "Please enter one or more words to search"
-		sys.exit()
